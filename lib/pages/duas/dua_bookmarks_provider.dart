28026745e24ff78305b85e392a6360f4031f114b
hamzadev2xx3 <flutterdeveloperhamzaa@gmail.com>
2023-06-09 17:11:57 +0500

    duafav9623

--- lib/pages/duas/dua_bookmarks_provider.dart
+++ lib/pages/duas/dua_bookmarks_provider.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:nour_al_quran/pages/quran/pages/recitation/reciter/player/player_provider.dart';
 import 'package:nour_al_quran/shared/database/quran_db.dart';
+import 'package:provider/provider.dart';
 
 import '../../shared/entities/bookmarks_dua.dart';
+import 'dua_detailed.dart';
 
 class BookmarkProviderDua extends ChangeNotifier {
   final List _bookmarkList = Hive.box('myBox').get('bookmarks1') ?? [];
   List get bookmarkList => _bookmarkList;
 
-  void removeBookmark(int duaId, int duaCategory) {
-    QuranDatabase().removeduaBookmark(duaId, duaCategory);
-    // print(_bookmarkList.indexWhere((element) => element.surahId == surahId && element.verseId == verseId));
-    _bookmarkList.removeWhere((element) =>
-        element.duaId == duaId && element.duaCategory == duaCategory);
+  void removeBookmark(int duaId) {
+    QuranDatabase().removeduaBookmark(duaId);
+    _bookmarkList.removeWhere((element) => element.duaId == duaId);
     notifyListeners();
     Hive.box("myBox").put("bookmarks1", _bookmarkList);
   }
@@ -24,23 +25,26 @@ class BookmarkProviderDua extends ChangeNotifier {
     Hive.box("myBox").put("bookmarks1", _bookmarkList);
   }
 
-  // void goToQuranView(BookmarksDua bookmarks, BuildContext context) async{
-  //   if(bookmarks.isFromJuz!){
-  //     context.read<QuranProvider>().setJuzText(juzId: bookmarks.juzId!,title: bookmarks.juzName!,fromWhere: 2,isJuz: true,bookmarkPosition: bookmarks.bookmarkPosition);
-  //     /// if recitation player is on So this line is used to pause the player
-  //     Future.delayed(Duration.zero,()=>context.read<RecitationPlayerProvider>().pause(context));
-  //     Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-  //       return const QuranTextView();
-  //     },));
-  //   }else{
-  //     // coming from surah so isJuz already false
-  //     // coming from surah so JuzId already -1
-  //     context.read<QuranProvider>().setSurahText(surahId: bookmarks.surahId!,title:'سورة ${bookmarks.surahArabic}',fromWhere: 2,bookmarkPosition: bookmarks.bookmarkPosition);
-  //     /// if recitation player is on So this line is used to pause the player
-  //     Future.delayed(Duration.zero,()=>context.read<RecitationPlayerProvider>().pause(context));
-  //     Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-  //       return const QuranTextView();
-  //     },));
-  //   }
-  // }
+  void goToAudioPlayer(BookmarksDua bookmarks, BuildContext context) async {
+    // context.read<QuranProvider>().setJuzText(
+    //     juzId: bookmarks.juzId!,
+    //     title: bookmarks.juzName!,
+    //     fromWhere: 2,
+    //     isJuz: true,
+    //     bookmarkPosition: bookmarks.bookmarkPosition);
+
+    /// if recitation player is on So this line is used to pause the player
+    Future.delayed(Duration.zero,
+        () => context.read<RecitationPlayerProvider>().pause(context));
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) {
+        return const DuaDetail();
+      },
+    ));
+  }
 }
+
+// Navigator.of(context).pushNamed(
+//                             RouteHelper.duaDetailed,
+//                           );
+//                           duaProvider.gotoDuaPlayerPage(dua.id!, context);
